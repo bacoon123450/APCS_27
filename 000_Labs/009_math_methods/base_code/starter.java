70486e97e5bc7072b63f2e26d411b0cd5b0bcbd3
Bacoon123450 <elwoodtsnell@gmail.com>
2026-09-09 20:45:05 +0000

    update

--- 000_Labs/009_math_methods/base_code/starter.java
+++ 000_Labs/009_math_methods/base_code/starter.java
@@ -7,8 +7,13 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println("maximum = "+Math.max(13-5*11, 30%7*(-2)));
+		System.out.println("Square root = "+Math.sqrt(3*8+31%7));
+		System.out.println("Power = "+Math.pow(37/3, 35%21));
+		double steve = Math.pow(2,14%3);
+		double fred = Math.sqrt(2*6);
+		System.out.println("Max = "+Math.max(steve, fred));
+
+
 	}
 }
--- 000_Labs/010_random_method-static/base_code/starter.java
+++ 000_Labs/010_random_method-static/base_code/starter.java
@@ -8,8 +8,13 @@ import java.util.Random;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		int x = (int)(Math.random() * 9);
+		System.out.println("An integer between 0 and 9 is " + x);
+		int y = (int)(Math.random() * 100+1);
+		System.out.println("An integer between 1 and 100 is " + y);
+		System.out.println("A double between 2.5 and 3.5 is " +((Math.random() * 3.5)+2.4));
+		System.out.println("A double between 14 and 589 is " + ((Math.random() * 589)+14.5));
+
+
 	}
 }
